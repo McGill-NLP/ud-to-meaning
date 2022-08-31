6a952480cdcf204c6ce35ef76bca43c027cc3ef1
L Bradford <libbyib@gmail.com>
2022-08-31 17:32:37 -0400

    streamlining code for demos at the end of simpleparsing

--- simpleparsing.py
+++ simpleparsing.py
@@ -387,150 +387,94 @@ def simplifynodetyped(treenode):
         treenode.token['word_den'] = treenode.token['rel_den'](treenode.token['word_den']).simplify()
     return treenode
 
+# This function summarizes all the things we would want to do
+# to an input file in the demos below.
+# It takes the contents of a conllu, in string format, as input.
+# It doesn't return anything, but prints the sentence expressed by the input file,
+# and the computed denotation of the sentence.
+# It's only here to streamline demos. 
+def print_sentence_and_parse(testconllu):
+    testsentence=conllu.parse(testconllu)[0]
+    print(testsentence.metadata['text'])
+    # Preprocess the sentence
+    preprocessed = preprocess(testsentence)
+    # Then add semantic information to each node...
+    withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
+    withtypes = conllu.TokenList([add_type(token) for token in withdens])
+    # Then collapse all the nodes together!
+    simplified = simplifynodetyped(withtypes.to_tree())
+    simplified.token['word_den'].pretty_print()
+
 # MARK Demos with a few Conll files
 
 # basic transitive sentence
 # We read the file, and print its structure...
 with open("conllus\\the rider possesses the will.conll") as f:
     testconllu = f.read()
-testsentence=conllu.parse(testconllu)[0]
-print(testsentence.metadata['text'])
-# Preprocess the sentence
-preprocessed = preprocess(testsentence)
-# Then add semantic information to each node...
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-withtypes = conllu.TokenList([add_type(token) for token in withdens])
-# Then collapse all the nodes together!
-simplified = simplifynodetyped(withtypes.to_tree())
-simplified.token['word_den'].pretty_print()
+print_sentence_and_parse(testconllu)
+
 
 # adjectives and binarization
 with open("conllus\\the city enjoys a temperate climate.conll") as f:
     testconllu = f.read()
-testsentence=conllu.parse(testconllu)[0]
-print(testsentence.metadata['text'])
-preprocessed = preprocess(testsentence)
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-withtypes = conllu.TokenList([add_type(token) for token in withdens])
-simplified = simplifynodetyped(withtypes.to_tree())
-simplified.token['word_den'].pretty_print()
+print_sentence_and_parse(testconllu)
 
 
 # quantifiers
 with open("conllus\\each section has a theme.conll") as f:
     testconllu = f.read()
-testsentence=conllu.parse(testconllu)[0]
-print(testsentence.metadata['text'])
-preprocessed = preprocess(testsentence)
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-withtypes = conllu.TokenList([add_type(token) for token in withdens])
-simplified = simplifynodetyped(withtypes.to_tree())
-simplified.token['word_den'].pretty_print()
+print_sentence_and_parse(testconllu)
 
 
 # obliques!
 with open("conllus\\the first drops fell onto the parched stones from a cloudless blue sky.conll") as f:
     testconllu = f.read()
-testsentence=conllu.parse(testconllu)[0]
-print(testsentence.metadata['text'])
-preprocessed = preprocess(testsentence)
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-withtypes = conllu.TokenList([add_type(token) for token in withdens])
-simplified = simplifynodetyped(withtypes.to_tree())
-simplified.token['word_den'].pretty_print()
+print_sentence_and_parse(testconllu)
 
 
 # nominal modifiers
 with open("conllus\\after a brief period in frankfurt the family moved to basel.conll") as f:
     testconllu = f.read()
-testsentence=conllu.parse(testconllu)[0]
-print(testsentence.metadata['text'])
-preprocessed = preprocess(testsentence)
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-withtypes = conllu.TokenList([add_type(token) for token in withdens])
-simplified = simplifynodetyped(withtypes.to_tree())
-simplified.token['word_den'].pretty_print()
+print_sentence_and_parse(testconllu)
 
 
 # determinerless nouns!
 with open("conllus\\the first drops of rain fell onto the parched stones from a cloudless blue sky.conll") as f:
     testconllu = f.read()
-testsentence=conllu.parse(testconllu)[0]
-print(testsentence.metadata['text'])
-preprocessed = preprocess(testsentence)
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-withtypes = conllu.TokenList([add_type(token) for token in withdens])
-simplified = simplifynodetyped(withtypes.to_tree())
-simplified.token['word_den'].pretty_print()
+print_sentence_and_parse(testconllu)
 
 
 # determinerless nouns!
 with open("conllus\\pages hung in tatters from the sodden blue spine.conll") as f:
     testconllu = f.read()
-testsentence=conllu.parse(testconllu)[0]
-print(testsentence.metadata['text'])
-preprocessed = preprocess(testsentence)
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-withtypes = conllu.TokenList([add_type(token) for token in withdens])
-simplified = simplifynodetyped(withtypes.to_tree())
-simplified.token['word_den'].pretty_print()
+print_sentence_and_parse(testconllu)
 
 
 # determinerless nouns!
 with open("conllus\\successful stormtroopers share a high - five.conll") as f:
     testconllu = f.read()
-testsentence=conllu.parse(testconllu)[0]
-print(testsentence.metadata['text'])
-preprocessed = preprocess(testsentence)
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-withtypes = conllu.TokenList([add_type(token) for token in withdens])
-simplified = simplifynodetyped(withtypes.to_tree())
-simplified.token['word_den'].pretty_print()
+print_sentence_and_parse(testconllu)
 
 
 # determinerless nouns and numbers!
 with open("conllus\\smith offered three reasons.conll") as f:
     testconllu = f.read()
-testsentence=conllu.parse(testconllu)[0]
-print(testsentence.metadata['text'])
-preprocessed = preprocess(testsentence)
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-withtypes = conllu.TokenList([add_type(token) for token in withdens])
-simplified = simplifynodetyped(withtypes.to_tree())
-simplified.token['word_den'].pretty_print()
+print_sentence_and_parse(testconllu)
 
 
 # proper nouns as det+noun, allowing them to be modified!
 with open("conllus\\after a brief period in frankfurt the family moved to basel in switzerland.conll") as f:
     testconllu = f.read()
-testsentence=conllu.parse(testconllu)[0]
-print(testsentence.metadata['text'])
-preprocessed = preprocess(testsentence)
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-withtypes = conllu.TokenList([add_type(token) for token in withdens])
-simplified = simplifynodetyped(withtypes.to_tree())
-simplified.token['word_den'].pretty_print()
+print_sentence_and_parse(testconllu)
 
 
 # flat constructions!
 with open("conllus\\wikinews interviews meteorological experts on cyclone phalin.conll") as f:
     testconllu = f.read()
-testsentence=conllu.parse(testconllu)[0]
-print(testsentence.metadata['text'])
-preprocessed = preprocess(testsentence)
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-withtypes = conllu.TokenList([add_type(token) for token in withdens])
-simplified = simplifynodetyped(withtypes.to_tree())
-simplified.token['word_den'].pretty_print()
+print_sentence_and_parse(testconllu)
 
 
 # flat and compound constructions, with special case for proper nouns
 with open("conllus\\texas student ahmed mohamed inspires social movement.conll") as f:
     testconllu = f.read()
-testsentence=conllu.parse(testconllu)[0]
-print(testsentence.metadata['text'])
-preprocessed = preprocess(testsentence)
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-withtypes = conllu.TokenList([add_type(token) for token in withdens])
-simplified = simplifynodetyped(withtypes.to_tree())
-simplified.token['word_den'].pretty_print()
+print_sentence_and_parse(testconllu)
