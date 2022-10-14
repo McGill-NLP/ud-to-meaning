8f53605284e0ac8b4fa8cb4e8b1205e0c39c52c1
L Bradford <libbyib@gmail.com>
2022-10-14 16:58:11 -0400

    streamline checking against PMB and add some demos

--- pmbdemos.py
+++ pmbdemos.py
@@ -11,21 +11,11 @@ from simpleparsing import *
 #####       because they are very big.
 #####       You can get them yourself and change the pmbpath below.
 
-pmbpath = "pmb-gold-english"
-datapointpath = r"\p00\d0712"
-
-# Get the raw text, tokens, and gold DRT structure.
-
-with open(pmbpath + datapointpath + r"\en.raw") as f:
-    datapointraw = f.read()
-print(datapointraw)
-
-with open(pmbpath + datapointpath + r"\en.tok.off") as f:
-    tokensraw = f.read()
-tokens = ['~'.join(x.split(' ')[3:]) for x in tokensraw.split('\n') if len(x) > 0]
-
-with open(pmbpath + datapointpath + r"\en.drs.clf") as f:
-    drsclfraw = f.read()
+# The NLP pipeline we will use to parse to UD
+stanzanlp = stanza.Pipeline(lang='en',
+                processors = 'tokenize,mwt,pos,lemma,depparse',
+                tokenize_pretokenized=True,
+                download_method=stanza.DownloadMethod.REUSE_RESOURCES)
 
 # This takes a raw clause format DRS (the type available in PMB data)
 # and outputs a DRS of the class from the DRT module.
@@ -46,22 +36,74 @@ def clf_to_drs(clfraw):
             conds.append(stmtsplit[0] + "(" + ",".join(argnames) + ")")
     return DrtExpression.fromstring("([" + ",".join(refs) + "],[" + ",".join(conds) + "])")
 
-pmb_drs = clf_to_drs(drsclfraw)
+# This function just makes demos easier.
+def compare_with_pmb(pmbpath, datapointpath, stanzanlp):
+    # Get the raw text, tokens, and gold DRT structure.
+    with open(pmbpath + datapointpath + r"\en.raw") as f:
+        datapointraw = f.read()
+    print(datapointraw)
 
-# Now we parse the tokens in UD, so we can use the UD-to-meaning parser.
-stanzanlp = stanza.Pipeline(lang='en', processors = 'tokenize,mwt,pos,lemma,depparse',tokenize_pretokenized=True)
-doc = stanzanlp([tokens])
+    with open(pmbpath + datapointpath + r"\en.tok.off") as f:
+        tokensraw = f.read()
+    tokens = ['~'.join(x.split(' ')[3:]) for x in tokensraw.split('\n') if len(x) > 0]
 
-ud_dict = doc.to_dict()[0]
-for x in ud_dict:
-    x['form'] = x['text']
-ud_parse = conllu.TokenList(ud_dict)
+    with open(pmbpath + datapointpath + r"\en.drs.clf") as f:
+        drsclfraw = f.read()
 
-# Then we apply the UD-based meaning program
+    pmb_drs = clf_to_drs(drsclfraw)
 
-preprocessed = preprocess(ud_parse)
-withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-simplified = simplifynodetyped(withdens.to_tree())
-simpleparsing_drs = simplified.token['word_dens']
+    # Now we parse the tokens in UD, so we can use the UD-to-meaning parser.
+    doc = stanzanlp([tokens])
 
-print("PMB DRS:\n"+pmb_drs.pretty_format()+"\n\nMy DRS:\n"+simpleparsing_drs[0][1].pretty_format())
+    ud_dict = doc.to_dict()[0]
+    for x in ud_dict:
+        x['form'] = x['text']
+    ud_parse = conllu.TokenList(ud_dict)
+    print("UD tree:\n")
+    ud_parse.to_tree().print_tree()
+
+    # Then we apply the UD-based meaning program
+    preprocessed = preprocess(ud_parse)
+    withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
+    simplified = simplifynodetyped(withdens.to_tree(),True)
+    simpleparsing_drs = simplified.token['word_dens']
+
+    print("\nPMB DRS:\n"+pmb_drs.pretty_format()+"\n\nMy DRS:\n"+simpleparsing_drs[0][1].pretty_format())
+
+    return pmb_drs, simpleparsing_drs
+
+# MARK here is where specific demos begin
+# Basic transitive sentence: "Kraft sold Celestial Seasonings"
+demo = compare_with_pmb("pmb-gold-english", r"\p00\d0712", stanzanlp)
+
+# Event subject: "Packing sucks"
+demo = compare_with_pmb("pmb-gold-english", r"\p00\d1666", stanzanlp)
+
+# Compound noun: "Kohl announced economy measures"
+demo = compare_with_pmb("pmb-gold-english", r"\p00\d1222", stanzanlp)
+
+# Intransitive sentence with adverb: "Mary just left"
+demo = compare_with_pmb("pmb-gold-english", r"\p00\d1589", stanzanlp)
+
+# Raising: "Tom seems conceited"
+demo = compare_with_pmb("pmb-gold-english", r"\p00\d1853", stanzanlp)
+
+# Intransitive sentence: "The storm abated"
+demo = compare_with_pmb("pmb-gold-english", r"\p00\d2069", stanzanlp)
+
+# Adverbial phrase: "Tom laughs a lot"
+# demo = compare_with_pmb("pmb-gold-english", r"\p00\d2218", stanzanlp)
+# (has a bug in PMB portion)
+
+# Adverb: "Tom draws well"
+demo = compare_with_pmb("pmb-gold-english", r"\p00\d2370", stanzanlp)
+
+# Quantifier: "Klava oversimplifies everything"
+# demo = compare_with_pmb("pmb-gold-english", r"\p00\d2540", stanzanlp)
+# (has a bug in PMB portion)
+
+# Control: "Tom started walking"
+demo = compare_with_pmb("pmb-gold-english", r"\p00\d3051", stanzanlp)
+
+# Prepositional phrase: "Pierce lives near Rossville Blvd"
+demo = compare_with_pmb("pmb-gold-english", r"\p00\d0761", stanzanlp)
