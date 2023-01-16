629f188c3bd256d86be07ed565f43821431edb32
L Bradford <libbyib@gmail.com>
2023-01-16 16:14:07 -0500

    Added failsafe that kills a parsing process after 5 minutes if it doesn't finish

--- pmbparsing.py
+++ pmbparsing.py
@@ -5,6 +5,8 @@ import conlluutils
 import preprocessing
 import treetodrs
 import clfutils
+import sys
+from multiprocessing import Process, Manager, freeze_support # To allow things to sometimes time out.
 
 stanzapipeline = None
 
@@ -13,6 +15,11 @@ def get_stanza():
     if stanzapipeline is None:
         stanzapipeline = stanza.Pipeline(lang='en', processors = 'tokenize,pos,lemma,depparse')
     return stanzapipeline
+
+def getalldens_proc_wrapper(tree, queue):
+    dens = treetodrs.getalldens(tree)
+    queue.put(dens)
+    return
 
 def parsepmb(pmbdir, outdir):
     pmbfiles = []
@@ -26,28 +33,8 @@ def parsepmb(pmbdir, outdir):
         os.mkdir(outdir)
     returnlist = []
         
-    i = 0
     # We'll process each datapoint in turn.
     for dpname in datapointpathdict.keys():
-        # There are certain individual PMB files that give it trouble; these are currently tracked here.
-        if dpname in ("..\\..\\..\\Downloads\\pmb-4.0.0\\data\\en\\gold\\p04\\d2024\\en",
-                        "..\\..\\..\\Downloads\\pmb-4.0.0\\data\\en\\gold\\p14\\d3314\\en",
-                        "..\\..\\..\\Downloads\\pmb-4.0.0\\data\\en\\gold\\p19\\d1455\\en",
-                        "..\\..\\..\\Downloads\\pmb-4.0.0\\data\\en\\gold\\p23\\d2513\\en",
-                        "..\\..\\..\\Downloads\\pmb-4.0.0\\data\\en\\gold\\p39\\d2847\\en",
-                        "..\\..\\..\\Downloads\\pmb-4.0.0\\data\\en\\gold\\p49\\d1795\\en",
-                        "..\\..\\..\\Downloads\\pmb-4.0.0\\data\\en\\gold\\p50\\d0704\\en",
-                        "..\\..\\..\\Downloads\\pmb-4.0.0\\data\\en\\gold\\p59\\d3229\\en",
-                        "..\\..\\..\\Downloads\\pmb-4.0.0\\data\\en\\gold\\p74\\d0808\\en",
-                        "..\\..\\..\\Downloads\\pmb-4.0.0\\data\\en\\gold\\p75\\d3043\\en",
-                        "..\\..\\..\\Downloads\\pmb-4.0.0\\data\\en\\gold\\p80\\d0554\\en"):
-            continue
-        # This bit of code lets you set certain indices to skip over
-        if i > -1:
-            i+=1
-        else:
-            i+=1
-            continue
         try:
             print(dpname)
             dpdir = outdir+'\\'+dpname.replace('\\','-')
@@ -56,8 +43,8 @@ def parsepmb(pmbdir, outdir):
             # read the raw file
             with open(datapointpathdict[dpname]['raw']) as f:
                 textraw = f.read()
+            # UD parse, and print to output file
             ud_parse = conlluutils.stanza_to_conllu(stanzanlp(textraw))
-            # UD parse to an output file, for testing
             ud_out = ud_parse.serialize()
             with open(dpdir+'\\'+'udparse.conll',mode='w') as f:
                 nlines = f.write(ud_out)
@@ -66,12 +53,23 @@ def parsepmb(pmbdir, outdir):
             with open(dpdir+'\\'+'preproc.conll',mode='w',encoding='utf8') as f:
                 nlines = f.write(preproc_out)
             # DRSs to an output file
-            ud_drss = treetodrs.getalldens(preprocessed)
+            #ud_drss = treetodrs.getalldens(preprocessed)
+            man = Manager()
+            q = man.Queue()
+            treetodrsproc = Process(target=getalldens_proc_wrapper,args=(preprocessed,q))
+            treetodrsproc.start()
+            treetodrsproc.join(timeout=300) # seconds
+            treetodrsproc.terminate()
+            if treetodrsproc.exitcode is None or q.empty():
+                print("Parsing this one took too long.")
+                continue
+            ud_drss = q.get()
             clflines = clfutils.drses_to_clf([y for x,y in ud_drss])
             with open(dpdir+'\\'+'drsoutput.clf', 'w',encoding='utf8') as f:
                 nlines = f.write('\n\n'.join(['\n'.join(x) for x in clflines]))
             # change both of them to simplified files
-            with open(dpdir+'\\'+'drsoutputsimple.clf', 'w',encoding='utf8') as f:
+            simplifieddrsname = dpdir+'\\'+'drsoutputsimple.clf'
+            with open(simplifieddrsname, 'w',encoding='utf8') as f:
                 nlines = f.write('\n\n'.join(['\n'.join(clfutils.simplify_clf(x)) for x in clflines]))
             pmbclf = ''
             with open(datapointpathdict[dpname]['drs']) as f:
@@ -80,14 +78,14 @@ def parsepmb(pmbdir, outdir):
             simplifiedpmbname = dpdir+'\\'+"pmbsimple.clf"
             with open(simplifiedpmbname, 'w',encoding='utf8') as f:
                 nlines = f.write('\n'.join(clfutils.simplify_clf(pmbclflines)))
-            returnlist.append((simplifiedpmbname,dpdir+'\\'+'drsoutputsimple.clf'))
+            returnlist.append((simplifiedpmbname,simplifieddrsname))
         except Exception as e:
-            print(e)
             continue
     return returnlist
     # TODO multiprocessing goes here
 
 if __name__ == "__main__":
+    freeze_support()
     # TODO this command line tool
     # when called from the command line it needs the exact same inputs lol
     print("I promise I'll implement this :)")
