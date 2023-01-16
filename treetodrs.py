9ae04b9e98d73a376bece71ffa713aeb6296dae9
L Bradford <libbyib@gmail.com>
2023-01-16 12:24:19 -0500

    Majorly reorganized scripts, but it's more modular and easier to find things now

--- treetodrs.py
+++ treetodrs.py
@@ -1,7 +1,6 @@
 #from nltk.inference import TableauProver # helps resolve anaphora
 from nltk.sem.drt import * # all the DRT things
 from nltk.sem.logic import unique_variable # helps for manipulating DRT expressions
-from pptree import Node # helps print traces nicely
 import graphviz # for displaying traces nicely
 from pptree import print_tree # helps print traces nicely
 import conllu # reading ConLL-U files
@@ -9,188 +8,12 @@ import os # changing working directory
 import copy # deep-copying Tokens and and TokenLists
 # whatever the working directory is! on my computer it is this.
 os.chdir('C:\\Users\\Lola\\OneDrive\\UDepLambda\\computer code')
-# the semtypes module should be in the working directory now
+# the local modules and files should be in the working directory now
 from semtypes import *
-# (also the folder "conllus" for the demos later)
+from conlluutils import *
+from preprocessing import *
 
-# MARK Functions to facilitate manipulating tokens and tokenlists
-
-# This function takes a TokenTree as input,
-# and returns a new TokenTree with most data the same as the original TokenTree,
-# with two differences:
-# first, the new TokenTree now has no children,
-# and second, the "form" and "lemma" of the TokenTree's token are now
-# concatenations of the existing "form" and "lemma" with those of all children
-# and their descendants as well.
-def concat_with_all_dependents(treenode):
-    treenode = copy.deepcopy(treenode)
-    for child in treenode.children:
-        child = concat_with_all_dependents(child)
-        if child.token['id'] < treenode.token['id']:
-            treenode.token['form'] = child.token['form'] + '_' + treenode.token['form']
-            treenode.token['lemma'] = child.token['lemma'] + '_' + treenode.token['lemma']
-        else:
-            treenode.token['form'] = treenode.token['form'] + '_' + child.token['form']
-            treenode.token['lemma'] = treenode.token['lemma'] + '_' + child.token['lemma']
-    treenode.children = []
-    return treenode
-
-# This function takes a TokenTree as input,
-# and returns a TokenList with the tokens from this tree
-# and all its children/descendants,
-# sorted by their ids.
-# NOTE: It uses the same actual tokens, not copies!!
-def tree_to_tokenlist(treenode):
-    if len(treenode.children) == 0:
-        return [treenode.token]
-    else:
-        answer = [treenode.token]
-        for child in treenode.children:
-            answer = answer + tree_to_tokenlist(child)
-        return conllu.TokenList(sorted(answer, key = lambda x:x['id']))
-
-# This function takes a TokenList as input,
-# and returns a new TokenList,
-# mostly the same as the input,
-# but with new id's starting at 1 and increasing by 1 with each subsequent word,
-# and all heads updated to match the new id's.
-# Handy when you've been adding tokens and want to smooth everything out.
-def reindex_tokenlist(sentence):
-    sentence = copy.deepcopy(sentence)
-    i = 0
-    for token in sentence:
-        i += 1
-        if 'misc' not in token.keys() or not token['misc']:
-            token['misc'] = {}
-        token['misc'].update(oldid = token['id'])
-        token['id'] = i
-    for token in sentence:
-        newhead = 0
-        for token2 in sentence:
-            if token2['misc']['oldid'] == token['head']:
-                newhead = token2['id']
-        token['head'] = newhead
-    for token in sentence:
-        del token['misc']['oldid']
-    return sentence
-
-# MARK Functions to preprocess sentences, adding posited null determiners and such.
-
-# This function is a hack,
-# but it changes any "flat" dependency relations
-# with head a NOUN and dependent a PROPN
-# into "nmod"...
-# or into "flat" dependent on a previous PROPN
-# if there are multiple on the same NOUN
-# ... it just makes the denotations come out nicer.
-# It takes a TokenList as input,
-# and returns a new TokenList with the change.
-def switch_propnflattonmod(sentence):
-    sentence = copy.deepcopy(sentence)
-    seenheads = {}
-    for token in sentence:
-        if token['upos'] == 'PROPN' and token['deprel'] == 'flat':
-            if sentence.filter(id=token['head'])[0]['upos'] == 'NOUN':
-                if token['head'] in seenheads.keys():
-                    token['head'] = seenheads[token['head']]
-                else:
-                    token['deprel'] = 'nmod'
-                    seenheads[token['head']] = token['id']
-                    token['upos'] = 'PROPN-MOD'
-    return sentence
-
-# This function flattens a given relation.
-# That is, it takes a TokenTree
-# and a relation (a string corresponding to a value of the "deprel" field) as input.
-# It returns a new TokenTree, just like the input, except
-# anywhere there used to be that relation,
-# - the dependent is completely flattened, with all _its_ dependents collapsed onto it in one token,
-# - and then the dependent is concatenated to the head as part of that token, and removed.
-# The result is that we essentially treat that relation as being between two parts of the same word,
-# and combine the parts of that word,
-# with POS and syntactic role corresponding to the relation's head.
-def flatten_relation_tree(treenode,relation):
-    treenode = copy.deepcopy(treenode)
-    childrentokeep = []
-    for child in treenode.children:
-        if child.token['deprel'] == relation:
-            child = concat_with_all_dependents(child)
-            if child.token['id'] < treenode.token['id']:
-                treenode.token['form'] = child.token['form'] + '_' + treenode.token['form']
-                treenode.token['lemma'] = child.token['lemma'] + '_' + treenode.token['lemma']
-            else:
-                treenode.token['form'] = treenode.token['form'] + '_' + child.token['form']
-                treenode.token['lemma'] = treenode.token['lemma'] + '_' + child.token['lemma']
-        else:
-            child = flatten_relation_tree(child, relation)
-            childrentokeep.append(child)
-    treenode.children = childrentokeep
-    return treenode
-
-# This function takes a TokenList as input
-# and performs the same operation as flatten_relation_tree on it,
-# returning the resulting TokenList with new indices.
-def flatten_relation_list(sentence,relation):
-    return reindex_tokenlist(
-                tree_to_tokenlist(
-                    flatten_relation_tree(
-                        sentence.to_tree(), relation
-                    )))
-
-
-# This function takes a TokenList as input
-# and returns a new TokenList,
-# very similar to the first,
-# but with null definite determiners inserted just before proper nouns without existing determiners,
-# and null indefinite determiners inserted just before common nouns without existing determiners.
-# Because of a hack involving the "flat" relation,
-# no determiner is inserted if the noun is related to its head with the "amod" relation.
-def add_nulldeterminers(sentence):
-    sentence = copy.deepcopy(sentence)
-    determinered = [sentence.filter(id=token['head'])[0] for token in sentence
-                        if (token['upos']=='DET' and token['deprel']=='det')]
-    determinerless = [token for token in sentence
-                        if (token['upos'] in ('NOUN','PROPN','PRON')
-                            and token not in determinered
-                            and token['deprel'] != "amod")]
-    for token in determinerless:
-        sentence.append(conllu.models.Token({'id':token['id']-0.5,
-                            'form':'∅-def' if token['upos'] in ('PROPN','PRON') else '∅-indf',
-                            'lemma':'∅-def' if token['upos'] in ('PROPN','PRON') else '∅-indf',
-                            'upos':'DET',
-                            'xpos':None,
-                            'feats':None,
-                            'head':token['id'],
-                            'deprel':'det',
-                            'deps': None,
-                            'misc': None
-                        }))
-    sentence = conllu.TokenList(sorted(sentence,key=lambda x:x['id']))
-    return reindex_tokenlist(sentence)
-
-# This combines all the previous steps in the correct order.
-# So, it changes flat relations between Nouns and Proper Nouns to "amod" relations (for semantic reasons),
-# flattens remaining "flat" relations,
-# flattens remaining "compound" relations,
-# and adds null determiners to any determiner-less nouns and proper nouns (except those modified by "amod"),
-# returning a new TokenList
-def preprocess(sentence):
-    return add_nulldeterminers(
-                flatten_relation_list(
-                    flatten_relation_list(
-                        flatten_relation_list(
-                            switch_propnflattonmod(
-                                sentence
-                            ),
-                        'flat'
-                        ),
-                    'compound'
-                    ),
-                'goeswith'
-                )
-            )
-
-# MARK all the word denotation templates, and relation denotations,
+# All the word denotation templates, and relation denotations,
 # are pairs of one SemType and one DRT expression (or template string for an expression)
 # We read them from the files.
 poslines = []
@@ -276,8 +99,6 @@ def compute_conj(den1, den2, semtype):
         xstr = str(x)
         recursivecallstr = str(compute_conj(den1(x).simplify(),den2(x).simplify(),semtype.get_right()))
         return DrtExpression.fromstring(rf'\{xstr}.{recursivecallstr}').simplify()
-
-# MARK actual semantic parsing
 
 # This function takes four inputs:
 # start is any type of thing, but usually a SemType
@@ -455,48 +276,10 @@ def simplifynodetyped(treenode, withtrace=False):
 
 # pass in a raw conllu or tokenlist
 # and get all of the denotations (or traces) computed for it
-def getalldens(rawconllu = None, tokenlist = None, withtrace = False):
-    if tokenlist is None:
-        tokenlist=conllu.parse(rawconllu)[0]
-    # Preprocess the sentence
-    preprocessed = preprocess(tokenlist)
-    # Then add semantic information to each node...
-    withdens = conllu.TokenList([add_denotation(token) for token in preprocessed])
-    # Then collapse all the nodes together!
+def getalldens(tokenlist, withtrace = False):
+    withdens = conllu.TokenList([add_denotation(token) for token in tokenlist])
     simplified = simplifynodetyped(withdens.to_tree(), withtrace)
     return simplified.token['word_dens']
-
-# Turn a trace
-# (output as a denotation of a sentence, from simplifynodetyped with withtrace=True)
-# into a tree structure that pptree can print.
-def tracetopptree(t, parent = None, withdrs = False):
-    if len(t) == 2:
-        name = f"{t[0]}:{t[1]}" if withdrs else str(t[0])
-        return Node(name,parent) if parent else Node(name)
-    elif len(t) == 3:
-        if t[2]['children']:
-            # has children
-            topnodename = ("[" if not parent else "") + f"{t[2]['form']}:{t[0]}" + (f":{t[1]}" if withdrs else "") + "]"
-            topnode = Node(topnodename,parent) if parent else Node(topnodename)
-            orignodename = f"[{t[2]['form']}:{t[2]['original'][0]}" + (f":{t[2]['original'][1]}" if withdrs else "")
-            orignode = Node(orignodename,topnode)
-            for child in t[2]['children']:
-                relnodename = (f"{child[1][2]['deprel']}:" if len(child[1]) > 2 else "") + f"{child[0][0]}" + (f":{child[0][1]}" if (withdrs and child[0][1]) else "")
-                relnode = Node(relnodename,orignode)
-                childnode = tracetopptree(child[1],relnode)
-            return topnode
-        else:
-            # no children
-            name = ("[" if not parent else "") + f"{t[2]['form']}:{t[0]}" + (f":{t[1]}" if withdrs else "") + "]"
-            return Node(name,parent) if parent else Node(name)
-    else:
-        pass
-
-# Actually printing the trace.
-# TODO might be better to use print_tree2 rather than pptree, to preserve child order
-def printtrace(t, withdrs = False, horizontal = True):
-    print_tree(tracetopptree(t,parent = None,withdrs = withdrs), horizontal = horizontal)
-
 
 # Turn a trace (as returned by simplifynodetyped) into a GraphViz directed graph
 # which can then be viewed, or styled, or whatever as desired.
