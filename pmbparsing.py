820e90c4a658429284b9a10997e5390d806fb374
L Bradford <libbyib@gmail.com>
2024-03-02 01:34:54 -0500

    update timeout length to 1200 seconds as we used in actual project

--- pmbparsing.py
+++ pmbparsing.py
@@ -88,7 +88,7 @@ def parsepmb_proc(datapointpathdict,outdir,queue,outlistfull,workingqueue,storet
                 dump = workingqueue.get_nowait()
             treetodrsproc = Process(target=getalldens_proc_wrapper,args=(preprocessed,workingqueue,storetypes,os.path.join(dpdir,'treetodrslog')),daemon=True)
             treetodrsproc.start()
-            timeoutsecs = 300 # seconds
+            timeoutsecs = 1200 # seconds
             treetodrsproc.join(timeout=timeoutsecs) #
             treetodrsproc.terminate()
             if treetodrsproc.exitcode is None or workingqueue.empty():
